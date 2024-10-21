d935f1456af824dab12cbe9bdd9bdad183ef3511
David Okaliwe <okaliwedavid@gmail.com>
2024-10-21 21:23:16 +0100

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#050759",
+        secondary: "#FED33C",
+        primaryalt: "#DAD9D9",
+        mainalt: "#7D7D7D",
+        mainRed: "#E30613",
+        main: "#434344",
+        main2: "#F3F3F3",
+        main3: "#B5B5B5",
+        green: "#4CA735",
+        greenBright: "#27E63A",
+        greenDeep: "#11AD21",
+        brightRed: "#EA4335"
+      },
+      fontFamily: {
+        'encode-sans': ["Encode Sans", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
-}
-
+};
