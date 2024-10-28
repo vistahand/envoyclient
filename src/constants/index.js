bff2c2651afb555848f833972f20461ac9ecfb89
David Okaliwe <okaliwedavid@gmail.com>
2024-10-28 22:51:44 +0000

    Updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,281 +1,15 @@
 export const navLinks = [
     {
-        id: "tiles",
-        title: "Tiles",
+        id: "services",
+        title: "Services",
         links: [
             {
-                name: "Ceramic Tiles",
-                route: "/products/tiles?type=Ceramic Tiles",
+                name: "Lorem Ipsum",
+                route: "",
             },
             {
-                name: "Porcelain Tiles",
-                route: "/products/tiles?type=Porcelain Tiles",
-            },
-            {
-                name: "Floor Tiles",
-                route: "/products/tiles?type=Floor Tiles",
-            },
-            {
-                name: "Wall Tiles",
-                route: "/products/tiles?type=Wall Tiles",
-            },
-            {
-                name: "Backsplash Tiles",
-                route: "/products/tiles?type=Backsplash Tiles",
-            },
-            {
-                name: "Bathroom Tiles",
-                route: "/products/tiles?type=Bathroom Tiles",
-            },
-            {
-                name: "Kitchen Tiles",
-                route: "/products/tiles?type=Kitchen Tiles",
-            },
-            {
-                name: "Indoor Tiles",
-                route: "/products/tiles?type=Indoor Tiles",
-            },
-            {
-                name: "Outdoor Tiles",
-                route: "/products/tiles?type=Outdoor Tiles",
-            },
-            {
-                name: "Pool Tiles",
-                route: "/products/tiles?type=Pool Tiles",
-            },
-            {
-                name: "Commercial Tiles",
-                route: "/products/tiles?type=Commercial Tiles",
-            },
-            {
-                name: "Residential Tiles",
-                route: "/products/tiles?type=Residential Tiles",
-            },
-            {
-                name: "Brick Tiles",
-                route: "/products/tiles?type=Brick Tiles",
-            },
-            {
-                name: "All and Others (Tiles)",
-                route: "/products/tiles?type=Other Tiles",
-            },
-        ],
-    },
-    {
-        id: "marble",
-        title: "Marble",
-        links: [
-            {
-                name: "Carrara Marble",
-                route: "/products/marble?type=Carrara Marble",
-            },
-            {
-                name: "Calacatta Marble",
-                route: "/products/marble?type=Calacatta Marble",
-            },
-            {
-                name: "Statuario Marble",
-                route: "/products/marble?type=Statuario Marble",
-            },
-            {
-                name: "Crema Marfill Marble",
-                route: "/products/marble?type=Crema Marfill Marble",
-            },
-            {
-                name: "Emperador Marble",
-                route: "/products/marble?type=Emperador Marble",
-            },
-            {
-                name: "Nero Marquina Marble",
-                route: "/products/marble?type=Nero Marquina Marble",
-            },
-            {
-                name: "Travertine Marble",
-                route: "/products/marble?type=Travertine Marble",
-            },
-            {
-                name: "Onyx Marble",
-                route: "/products/marble?type=Onyx Marble",
-            },
-            {
-                name: "Marble Slabs",
-                route: "/products/marble?type=Marble Slabs",
-            },
-            {
-                name: "Marble Tiles",
-                route: "/products/marble?type=Marble Tiles",
-            },
-            {
-                name: "All and Others (Marble)",
-                route: "/products/marble?type=Other Marble",
-            },
-        ],
-    },
-    {
-        id: "granite",
-        title: "Granite",
-        links: [
-            {
-                name: "Absolute Black Granite",
-                route: "/products/granite?type=Absolute Black Granite",
-            },
-            {
-                name: "Tan Brown Granite",
-                route: "/products/granite?type=Tan Brown Granite",
-            },
-            {
-                name: "Giallo Ornamental Granite",
-                route: "/products/granite?type=Giallo Ornamental Granite",
-            },
-            {
-                name: "Santa Cecilia Granite",
-                route: "/products/granite?type=Santa Cecilia Granite",
-            },
-            {
-                name: "Ubatuba Granite",
-                route: "/products/granite?type=Ubatuba Granite",
-            },
-            {
-                name: "Kashmir White Granite",
-                route: "/products/granite?type=Kashmir White Granite",
-            },
-            {
-                name: "Baltic Brown Granite",
-                route: "/products/granite?type=Baltic Brown Granite",
-            },
-            {
-                name: "Black Galaxy Granite",
-                route: "/products/granite?type=Black Galaxy Granite",
-            },
-            {
-                name: "Blue Pearl Granite",
-                route: "/products/granite?type=Blue Pearl Granite",
-            },
-            {
-                name: "Tiger Skin Granite",
-                route: "/products/granite?type=Tiger Skin Granite",
-            },
-            {
-                name: "Granite Slabs",
-                route: "/products/granite?type=Granite Slabs",
-            },
-            {
-                name: "All and Others (Granites)",
-                route: "/products/granite?type=Other Granite",
-            },
-        ],
-    },
-    {
-        id: "sanitary",
-        title: "Sanitary Wares",
-        links: [
-            {
-                name: "Toilets",
-                route: "/products/sanitary-wares?type=Toilets",
-            },
-            {
-                name: "Basins/Sinks",
-                route: "/products/sanitary-wares?type=Basins/Sinks",
-            },
-            {
-                name: "Showers",
-                route: "/products/sanitary-wares?type=Showers",
-            },
-            {
-                name: "Bidets",
-                route: "/products/sanitary-wares?type=Bidets",
-            },
-            {
-                name: "Urinals",
-                route: "/products/sanitary-wares?type=Urinals",
-            },
-            {
-                name: "Faucets and Taps",
-                route: "/products/sanitary-wares?type=Faucets and Taps",
-            },
-            {
-                name: "Accessories",
-                route: "/products/sanitary-wares?type=Accessories",
-            },
-            {
-                name: "Drainage and Plumbing Fittings",
-                route: "/products/sanitary-wares?type=Drainage and Plumbing Fittings",
-            },
-            {
-                name: "Water Filtration/Purification Systems",
-                route: "/products/sanitary-wares?type=Water Filtration/Purification Systems",
-            },
-        ],
-    },
-    {
-        id: "floorandwall",
-        title: "Floor and Wall Materials",
-        links: [
-            {
-                name: "Hardwood",
-                route: "/products/floor-and-wall-materials?type=Hardwood",
-            },
-            {
-                name: "Laminate",
-                route: "/products/floor-and-wall-materials?type=Laminate",
-            },
-            {
-                name: "Vinyl",
-                route: "/products/floor-and-wall-materials?type=Vinyl",
-            },
-            {
-                name: "Carpet",
-                route: "/products/floor-and-wall-materials?type=Carpet",
-            },
-            {
-                name: "Urinals",
-                route: "/products/floor-and-wall-materials?type=Urinals",
-            },
-            {
-                name: "Paint",
-                route: "/products/floor-and-wall-materials?type=Paint",
-            },
-            {
-                name: "Wallpaper",
-                route: "/products/floor-and-wall-materials?type=Wallpaper",
-            },
-            {
-                name: "Wood Panelling",
-                route: "/products/floor-and-wall-materials?type=Wood Panelling",
-            },
-            {
-                name: "Wainscoting",
-                route: "/products/floor-and-wall-materials?type=Wainscoting",
-            },
-        ],
-    },
-    {
-        id: "more",
-        title: "More Products",
-        links: [
-            {
-                name: "Marble Slabs",
-                route: "/products/marble-slab",
-            },
-            {
-                name: "Granite Slabs",
-                route: "/products/granite-slab",
-            },
-            {
-                name: "Doors",
-                route: "/products/doors",
-            },
-            {
-                name: "Other Products",
-                route: "/products/other-products",
-            },
-            {
-                name: "Installation Services",
-                route: "/products/installation",
-            },
-            {
-                name: "Consultation",
-                route: "/products/consultation",
+                name: "Lorem Ipsum",
+                route: "",
             },
         ],
     },
@@ -288,10 +22,6 @@ export const navLinks = [
                 route: "/about",
             },
             {
-                name: "Contact Us",
-                route: "/contact",
-            },
-            {
                 name: "Careers",
                 route: "/careers",
             },
@@ -302,6 +32,34 @@ export const navLinks = [
             {
                 name: "Privacy Policy",
                 route: "/privacypolicy",
+            },
+        ],
+    },
+    {
+        id: "resources",
+        title: "Resources",
+        links: [
+            {
+                name: "Lorem Ipsum",
+                route: "",
+            },
+            {
+                name: "Lorem Ipsum",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "support",
+        title: "Support",
+        links: [
+            {
+                name: "Lorem Ipsum",
+                route: "",
+            },
+            {
+                name: "Lorem Ipsum",
+                route: "",
             },
         ],
     },
