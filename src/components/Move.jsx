f46f1c21c302ca4bc9876a0b323ff0b94ffe21cc
David Okaliwe <okaliwedavid@gmail.com>
2024-10-31 00:20:11 +0000

    Updates

--- src/components/Move.jsx
+++ src/components/Move.jsx
@@ -7,30 +7,26 @@ import { moveCards } from '../constants';
 const MoveCard = ({ index, title, image, description }) => {
 
   return (
-    <motion.div className='bg-mainalt rounded-3xl hover:shadow-xl
+    <motion.div className='rounded-3xl hover:shadow-xl relative
     md:basis-1/3 ss:basis-1/2 basis-full h-[400px] flex navsmooth'
-    variants={fadeIn('', 'spring', index * 0.5, 0.75)}>
-      <div className='flex flex-col items-center justify-center
-      md:gap-10 ss:gap-8 gap-6 md:p-6 ss:p-10 p-8'>
-        <img
-          src={image}
-          alt='moveCard'
-        />
+      variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+      style={{
+        backgroundImage: image
+      }}
+    >
+      <div className='flex flex-col md:gap-4 ss:gap-2.5 absolute
+      gap-2 tracking- md:p-6 ss:p-10 p-8 bottom-0'>
+        <h1 className='text-white md:text-[30px] ss:text-[25px] 
+        text-[18px] font-bold md:leading-[2.2rem] max-w-[10ch]
+        ss:leading-[2.3rem] leading-[2.3rem]'>
+          {title}
+        </h1>
 
-        <div className='flex flex-col md:gap-4 ss:gap-2.5
-        gap-2 tracking-tight items-center justify-center text-center'>
-          <h1 className='text-primary md:text-[26px] ss:text-[20px] 
-          text-[15px] font-bold md:leading-[2.2rem]
-          ss:leading-[2.3rem] leading-[2.3rem]'>
-            {title}
-          </h1>
-
-          <p className='text-main font-medium md:leading-[20px]
-          ss:leading-[20px] leading-[18px] md:text-[15px] 
-          ss:text-[15px] text-[13px]'>
-            {description}
-          </p>
-        </div>
+        <p className='text-white md:leading-[20px]
+        ss:leading-[20px] leading-[18px] md:text-[14px] 
+        ss:text-[14px] text-[13px]'>
+          {description}
+        </p>
       </div>
     </motion.div>
   )
