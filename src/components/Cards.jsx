9043bef5d378600f1f586679bbf9cdfc0dbfff84
David Okaliwe <okaliwedavid@gmail.com>
2024-11-03 15:57:27 +0000

    Updates

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,26 +8,26 @@ const CardItem = ({ index, title, image, description }) => {
 
     return (
         <motion.div className='bg-mainalt rounded-3xl hover:shadow-xl
-        h-[400px] flex navsmooth'
+        md:h-[400px] ss:h-[370px] h-[320px] flex navsmooth'
         variants={fadeIn('', 'spring', index * 0.5, 0.75)}>
             <div className='flex flex-col items-center justify-center
-            md:gap-10 ss:gap-8 gap-6 md:p-6 ss:p-10 p-8'>
+            md:gap-10 ss:gap-8 gap-4 md:p-6 ss:p-10 p-8'>
                 {React.createElement(image, {
-                    className: `md:w-[6rem] ss:w-[20px] w-[18px] h-auto 
-                    object-contain text-primary`
+                    className: `w-[6rem] h-auto object-contain 
+                    text-primary`
                 })}
 
-                <div className='flex flex-col md:gap-4 ss:gap-2.5
-                gap-2 tracking-tight items-center justify-center text-center'>
-                    <h1 className='text-primary md:text-[26px] ss:text-[20px] 
-                    text-[15px] font-bold md:leading-[2.2rem]
-                    ss:leading-[2.3rem] leading-[2.3rem]'>
+                <div className='flex flex-col md:gap-4 ss:gap-2.5 gap-2.5
+                tracking-tight items-center justify-center text-center'>
+                    <h1 className='text-primary md:text-[26px] ss:text-[24px] 
+                    text-[23px] font-bold md:leading-[2.2rem]
+                    ss:leading-[1.9rem] leading-[1.6rem]'>
                         {title}
                     </h1>
 
                     <p className='text-main font-medium md:leading-[20px]
-                    ss:leading-[20px] leading-[18px] md:text-[15px] 
-                    ss:text-[15px] text-[13px]'>
+                    ss:leading-[20px] leading-[19px] md:text-[15px] 
+                    ss:text-[15px] text-[14px]'>
                         {description}
                     </p>
                 </div>
@@ -38,8 +38,8 @@ const CardItem = ({ index, title, image, description }) => {
 
 const Cards = () => {
   return (
-    <section className='relative w-full min-h-[500px] mx-auto flex
-    items-center'>
+    <section className='relative w-full md:min-h-[500px] ss:min-h-[500px]
+    min-h-[1100px] mx-auto flex items-center'>
         <div className='flex w-full'>
             <div className='grid md:grid-cols-3 ss:grid-cols-2 md:gap-12 
             ss:gap-10 gap-8 w-full'>
