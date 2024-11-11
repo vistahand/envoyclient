71168624bdbad03ca639375b86245a38aa3aa6e2
David Okaliwe <okaliwedavid@gmail.com>
2024-11-11 18:18:47 +0000

    Updates

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,15 @@
+import {
+    GetStartedForm
+} from '../pages';
+
+
+const GetStarted = () => {
+
+    return (
+        <div className='font-manrope'>
+           <GetStartedForm />
+        </div>
+    )
+};
+
+export default GetStarted;
