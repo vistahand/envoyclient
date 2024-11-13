b62547153b654b2b61d1c16407b053463525fb53
David Okaliwe <okaliwedavid@gmail.com>
2024-11-13 20:42:29 +0000

    Updates

--- src/pages/GetStartedForm.jsx
+++ src/pages/GetStartedForm.jsx
@@ -140,6 +140,7 @@ const GetStartedForm = () => {
                         >
                             Ship within your country
                         </p>
+                        
                     </div>
                 </div>
             </div>
