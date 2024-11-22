a04d1e6e950adb1da3809d387e924ede4ca47fdc
David Okaliwe <okaliwedavid@gmail.com>
2024-11-22 17:25:23 +0000

    Updates

--- src/pages/PackageDescribe.jsx
+++ src/pages/PackageDescribe.jsx
@@ -209,6 +209,7 @@ const PackageDescribe = ({ onPrev, selectedTab}) => {
                                 ${formik.values.packageWeight ? 'z-10 px-2' : ''}
                                 `}
                                 >
+                                    
                                     Weight of package (kg)
                                 </label>
                             </div>
