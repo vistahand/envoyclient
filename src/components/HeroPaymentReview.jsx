3c44cd24213bee954fa9776172ee6463826848ea
David Okaliwe <okaliwedavid@gmail.com>
2024-12-10 22:43:03 +0000

    Updates

--- src/components/HeroPaymentReview.jsx
+++ src/components/HeroPaymentReview.jsx
@@ -4,21 +4,30 @@ import { textVariant } from '../utils/motion';
 
 const HeroPaymentReview = () => {
   return (
-    <section className='relative w-full md:min-h-[100px] ss:min-h-[100px] 
-    items-center flex'>
-      <div className='relative items-center w-full max-w-[86rem]
-      md:mt-24 ss:mt-8 mt-16 flex'>
+    <section className='w-full md:min-h-[100px] ss:min-h-[100px] 
+    md:mt-12 ss:mt-12 mt-8 items-center'>
+      <div className='items-center w-full'>
         <motion.div variants={textVariant()}
-        className='flex md:gap-4 ss:gap-2 gap-2 text-main 
-        md:text-[15px] ss:text-[15px] text-[13px]'
-        >
-          <a href='/' className='hover:text-secondary'>
-            Home
-          </a>
+        className='flex md:w-[38%] ss:w-[38%] w-full md:gap-6 ss:gap-4 
+        gap-3 items-center'>
+          <p className='text-white font-bold md:text-[14px] cursor-pointer
+          ss:text-[14px] text-[13px] bg-primary py-2 px-6 rounded-full'>
+            Review
+          </p>
+          
+          <div className='w-full h-[1px] bg-main6'/>
 
-          <p> {'>'} </p>
+          <p className='text-main2 font-semibold md:text-[14px] 
+          ss:text-[14px] text-[13px]'>
+            Pay
+          </p>
 
-          <p>Cart</p>
+          <div className='w-full h-[1px] bg-main6'/>
+
+          <p className='text-main2 font-semibold md:text-[14px] 
+          ss:text-[14px] text-[13px]'>
+            Finish
+          </p>
         </motion.div>
       </div>
     </section>  
