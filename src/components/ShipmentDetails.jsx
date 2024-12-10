3b3dff368cfecabfcde73498ef02fe68eed64ce5
David Okaliwe <okaliwedavid@gmail.com>
2024-12-10 23:50:43 +0000

    Updates

--- src/components/ShipmentDetails.jsx
+++ src/components/ShipmentDetails.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { SectionWrapper } from "../hoc";
+import { motion } from "framer-motion";
 
 const ShipmentDetails = () => {
   return (
-    <div>ShipmentDetails</div>
+    <section className='w-full flex md:min-h-[1500px] ss:min-h-[1500px]
+    min-h-[1500px]'>
+      <div className="w-full md:flex-row ss:flex-row flex-col">
+        <div>
+          
+        </div>
+      </div>
+    </section>
   )
-}
+};
 
-export default ShipmentDetails
+export default SectionWrapper(ShipmentDetails, '');
