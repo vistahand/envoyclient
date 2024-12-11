baa4b1a87d96288322592a7d7ee5e753c2a86d54
David Okaliwe <okaliwedavid@gmail.com>
2024-12-11 02:30:57 +0000

    Updates

--- src/components/ShipmentDetails.jsx
+++ src/components/ShipmentDetails.jsx
@@ -2,6 +2,7 @@ import { SectionWrapper } from "../hoc";
 import { motion } from "framer-motion";
 import { ReactComponent as LocalIcon } from '../assets/loc-ship.svg';
 import { ReactComponent as InternationalIcon } from '../assets/int-ship.svg';
+import { HiOutlineArrowRight } from "react-icons/hi";
 
 const ShipmentDetails = () => {
   return (
@@ -36,16 +37,16 @@ const ShipmentDetails = () => {
           </div>
         </div>
 
-        <div className="md:w-[45%] ss:w-[45%]">
-          <div className="bg-primary1 md:p-8 ss:p-8 p-6 flex flex-col 
-          rounded-lg gap-5">
-            <h1 className="font-bold md:text-[15px] ss:text-[15px] text-[14px]
+        <div className="md:w-[55%] ss:w-[55%]">
+          <div className="bg-primary1 md:p-10 ss:p-10 p-8 flex flex-col 
+          rounded-2xl gap-6">
+            <h1 className="font-bold md:text-[16px] ss:text-[16px] text-[14px]
             tracking-tight text-main2">
               Payment Summary
             </h1>
 
-            <div className="flex flex-col w-full gap-3 md:text-[14px] 
-            ss:text-[14px] text-[13px]">
+            <div className="flex flex-col w-full gap-2.5 md:text-[13px] 
+            ss:text-[13px] text-[12px] tracking-tight">
               <div className="flex justify-between items-center w-full
               text-main2 font-medium">
                 <p>
@@ -90,7 +91,7 @@ const ShipmentDetails = () => {
             </div>
 
             <div className="flex justify-between items-center w-full">
-              <p className="md:text-[14px] ss:text-[14px] text-[13px]">
+              <p className="md:text-[13px] ss:text-[13px] text-[12px]">
                 Subtotal:
               </p>
 
@@ -106,12 +107,24 @@ const ShipmentDetails = () => {
             <div className='w-full h-[1px] bg-main5'/>
 
             <p className="text-main4 md:text-[11px] ss:text-[11px] 
-            text-[10px] font-medium md:leading-[20px] ss:leading-[20px]
-            leading-[18px]">
+            text-[10px] font-medium md:leading-[16px] ss:leading-[15px]
+            leading-[14px]">
               This figure does not include any other extra fees that may 
               be incurred via delayed orders, payment gateway fees, etc. 
               For more details, <span className="text-primary font-semibold">read our terms of usage here.</span>
             </p>
+
+            <a href='/createshipment-paymentreview'
+            className='bg-primary text-[13px] py-3 w-full flex
+            text-white rounded-full grow4 cursor-pointer items-center 
+            gap-3 justify-center'
+            >
+              <p className='font-[500]'>
+                Proceed to Payment
+              </p>
+              
+              <HiOutlineArrowRight className='text-[14px]'/>
+            </a>
           </div>
         </div>
       </div>
