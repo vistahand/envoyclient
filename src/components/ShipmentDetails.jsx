79e22e918c2b2043d8a331faa5a626b93ad07447
David Okaliwe <okaliwedavid@gmail.com>
2024-12-11 02:04:02 +0000

    Updates

--- src/components/ShipmentDetails.jsx
+++ src/components/ShipmentDetails.jsx
@@ -37,14 +37,70 @@ const ShipmentDetails = () => {
         </div>
 
         <div className="md:w-[45%] ss:w-[45%]">
-          <div className="bg-primary1 p-6 flex flex-col rounded-lg gap-5">
+          <div className="bg-primary1 md:p-8 ss:p-8 p-6 flex flex-col 
+          rounded-lg gap-5">
             <h1 className="font-bold md:text-[15px] ss:text-[15px] text-[14px]
             tracking-tight text-main2">
               Payment Summary
             </h1>
 
-            <div className="">
+            <div className="flex flex-col w-full gap-3 md:text-[14px] 
+            ss:text-[14px] text-[13px]">
+              <div className="flex justify-between items-center w-full
+              text-main2 font-medium">
+                <p>
+                  Shipment Cost
+                </p>
 
+                <p>
+                  <span className='line-through'>
+                    N
+                  </span>
+                  365,000.00
+                </p>
+              </div>
+
+              <div className="flex justify-between items-center w-full
+              text-main2 font-medium">
+                <p>
+                  VAT (7.5%)
+                </p>
+
+                <p>
+                  <span className='line-through'>
+                    N
+                  </span>
+                  27,375.00
+                </p>
+              </div>
+
+              <div className="flex justify-between items-center w-full
+              text-main2 font-medium">
+                <p>
+                  Insurance Coverage (Basic)
+                </p>
+
+                <p>
+                  <span className='line-through'>
+                    N
+                  </span>
+                  20,000.00
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-between items-center w-full">
+              <p className="md:text-[14px] ss:text-[14px] text-[13px]">
+                Subtotal:
+              </p>
+
+              <p className="text-primary md:text-[20px] ss:text-[20px] 
+              text-[17px] font-bold">
+                <span className='line-through'>
+                  N
+                </span>
+                412,375.00
+              </p>
             </div>
           </div>
         </div>
