b85f8e84f310cc4fa7eac536c309569d3bb86bee
David Okaliwe <okaliwedavid@gmail.com>
2024-12-12 20:18:57 +0000

    Updates

--- src/components/HeroPaymentReview.jsx
+++ src/components/HeroPaymentReview.jsx
@@ -8,8 +8,8 @@ const HeroPaymentReview = () => {
     md:p-0 ss:pl-16 ss:pr-16 pl-6 pr-6'>
       <div className='items-center w-full max-w-[68rem]'>
         <motion.div variants={textVariant()}
-        className='flex md:w-[38%] ss:w-[38%] w-full md:gap-6 ss:gap-4 
-        gap-3 items-center'>
+        className='flex md:w-[38%] ss:w-[38%] w-[90%] md:gap-6 ss:gap-5 
+        gap-4 items-center'>
           <p className='text-white font-bold md:text-[14px] cursor-pointer
           ss:text-[14px] text-[13px] bg-primary py-2 px-6 rounded-full'>
             Review
