93729b61afb6cd2427d37abbda1138ebd6bec0a8
David Okaliwe <okaliwedavid@gmail.com>
2024-12-13 13:24:04 +0000

    Updates

--- src/components/ShippingModal.jsx
+++ src/components/ShippingModal.jsx
@@ -18,26 +18,26 @@ const ShippingModal = ({ onClose }) => {
         className="fixed inset-0 flex items-center justify-center
         bg-black bg-opacity-40 z-50">
           <div className='max-w-[68rem] w-full flex md:justify-center 
-          ss:justify-center'>
+          ss:justify-center md:mx-0 ss:mx-0 mx-5'>
             <motion.div
             initial={{ y: 0, opacity: 0.7 }}
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: 10, opacity: 0 }}
             transition={{ duration: 0.1 }}
             className="bg-white padphone md:rounded-2xl ss:rounded-2xl
-            rounded-xl shadow-xl flex flex-col gap-5
-            md:h-auto ss:h-auto h-[100%] overflow-auto items-center 
-            relative md:w-[85%] w-[85%] md:py-6 md:px-12 ss:py-6 ss:px-12 py-4
+            rounded-xl shadow-xl flex flex-col gap-5 md:w-[85%] ss:w-[85%]
+            w-full md:h-[80%] ss:h-[80%] h-[100%] overflow-auto items-center 
+            relative  md:py-6 md:px-12 ss:py-6 ss:px-12 py-4
             px-5">
               <div className='flex justify-between items-center w-full'>
                 <h1 className="md:text-[30px] ss:text-[25px] 
-                text-[22px] tracking-tight font-bold text-main2">
+                text-[20px] tracking-tight font-bold text-main2">
                   Edit Sender Information
                 </h1>
 
                 <BsX 
-                  className='w-[3.2rem] h-auto text-redClose bg-redCircle
-                  p-2.5 rounded-full cursor-pointer grow2'
+                  className='md:w-[3.2rem] ss:w-[3.2rem] w-[2rem] h-auto 
+                  text-redClose bg-redCircle md:p-2.5 ss:p-2.5 p-1.5 rounded-full cursor-pointer grow2'
                   strokeWidth={0.2}
                   onClick={() => {
                     onClose();
