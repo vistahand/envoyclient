dbc84d948217b727ed6bfb11baadaeda3f99b5b5
David Okaliwe <okaliwedavid@gmail.com>
2024-12-14 01:04:49 +0000

    Updates

--- src/pages/ShipmentReview.jsx
+++ src/pages/ShipmentReview.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import {
     ShipmentDetails,
     ShipmentPay,
@@ -6,21 +6,33 @@ import {
 
 const ShipmentReview = () => {
     const [currentStep, setCurrentStep] = useState(1);
+    const [initialLoad, setInitialLoad] = useState(true);
     // const [selectedTab, setSelectedTab] = useState('international');
     // const [senderTab, setSenderTab] = useState('individual');
+    const sectionRef = useRef(null); 
+
+    useEffect(() => {
+        if (!initialLoad && sectionRef.current) {
+            const offset = -180; 
+            const y = sectionRef.current.getBoundingClientRect().top + window.pageYOffset + offset;
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+    }, [currentStep, initialLoad]);
 
     const handleNextStep = (tab) => {
         setCurrentStep(currentStep + 1);
+        setInitialLoad(false);
         // setSelectedTab(tab);
     };
 
     const handlePreviousStep = (tab, ) => {
         setCurrentStep(currentStep - 1);
+        setInitialLoad(false);
         // setSelectedTab(tab);
     };
 
     return (
-        <div className='font-manrope'>
+        <div ref={sectionRef} className='font-manrope'>
             {currentStep === 1 && <ShipmentDetails onNext={handleNextStep} />}
             {currentStep === 2 && <ShipmentPay onPrev={handlePreviousStep} onNext={handleNextStep} />}
         </div>
