dac4a4b8fb10b2e1751b393a55951b3c16324edf
David Okaliwe <okaliwedavid@gmail.com>
2024-12-23 13:12:00 +0000

    Updates

--- src/dashboard/user/Register.jsx
+++ src/dashboard/user/Register.jsx
@@ -46,27 +46,28 @@ const Register = () => {
   return (
     <section className='relative w-full flex items-center'>
       <div className='flex md:flex-row ss:flex-row flex-col w-full 
-      md:justify-between md:gap-14 ss:gap-8 gap-6'>
+      md:justify-between md:gap-20 ss:gap-8 gap-6 h-full'>
         <div className='md:w-[50%] w-full flex flex-col md:py-10'>
           <div className='w-full flex flex-col h-full md:gap-6 ss:gap-6 
           gap-5'>
             <div className='flex flex-col gap-1 w-full'>
               <h1 className='text-primary font-semibold md:text-[37px]
-              ss:text-[35px] text-[28px] tracking-tight'>
+              ss:text-[35px] text-[32px] tracking-tight mobline'>
                 Your experience begins here!
               </h1>
 
               <h2 className="md:text-[15px] ss:text-[16px] text-[14px]  
               tracking-tight font-medium text-main4 md:leading-[22px]
-              ss:leading-[22px] leading-[20px]">
+              ss:leading-[22px] leading-[20px] md:mt-0 ss:mt-0 mt-1">
                 Provide a few details about yourself so we can get you 
-                started. Already got an account?
+                started. Already got an account? <br className="hidden
+                ss:flex"/>
                 <a href='/login' className='text-primary font-bold 
                 hover:text-secondary navsmooth'> Log in here</a>
               </h2>
             </div>
 
-            <div className="flex justify-between items-center w-full 
+            <div className="flex md:justify-between items-center w-full 
             md:gap-5 ss:gap-5 gap-4">
               <div className="flex items-center gap-2 p-3.5 rounded-full
               border border-main7 cursor-pointer grow4">
@@ -130,7 +131,7 @@ const Register = () => {
             <div className='w-full'>
               <form className='flex flex-col w-full gap-5'>
                 <div className='flex flex-col md:w-[80%] ss:w-[80%] w-full 
-                md:gap-4 ss:gap-4 gap-3 mt-1'>
+                md:gap-4 ss:gap-4 gap-4 mt-1'>
                   <div className="relative flex flex-col">
                     <input
                       type="text"
@@ -281,7 +282,7 @@ const Register = () => {
                   </div>
                 </div>
 
-                <p className='md:text-[13px] ss:text-[14px] text-[13px]  
+                <p className='md:text-[13px] ss:text-[14px] text-[12px]  
                 tracking-tight font-medium text-main4 md:leading-[18px]
                 ss:leading-[19px] leading-[18px] mt-1'>
                   By clicking on Register, you agree to Envoy Angel's Terms 
@@ -323,8 +324,8 @@ const Register = () => {
           </div>
         </div>
 
-        <div className='w-[50%] h-auto md:flex hidden'>
-          <div className='w-full h-auto relative rounded-2xl'>
+        <div className='w-[50%] md:flex hidden'>
+          <div className='w-full relative rounded-2xl'>
             <img
               src={register}
               alt='register'
