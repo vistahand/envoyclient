652bcb58b2481a9fd0ff5524d3d0056a1aaa71b6
David Okaliwe <okaliwedavid@gmail.com>
2024-12-24 11:35:40 +0000

    Updates

--- src/dashboard/user/index.js
+++ src/dashboard/user/index.js
@@ -2,8 +2,13 @@ import RegisterStart from './RegisterStart';
 import RegisterConfirm from './RegisterConfirm';
 import RegisterFinish from './RegisterFinish';
 
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
 export {
     RegisterStart,
     RegisterConfirm,
     RegisterFinish,
+    Sidebar,
+    Navbar,
 }
--- src/scenes/UserDashHome.jsx
+++ src/scenes/UserDashHome.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import {
+  Navbar,
+  Sidebar,
+} from '../dashboard/user';
+
+import { Helmet } from 'react-helmet';
 
 const UserDashHome = () => {
-  return (
-    <div>UserDashHome</div>
-  )
-}
 
-export default UserDashHome
+  return (
+    <div className='font-manrope'>
+      <Helmet>
+        <title>User | Envoy Angel Shipping and Logistics</title>
+        <meta name="description" content="Content" />
+      </Helmet>
+
+      <div className='footer'>
+        <Sidebar />
+      </div>
+
+      <Navbar />
+
+    </div>
+  )
+};
+
+export default UserDashHome;
