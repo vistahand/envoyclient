5312d97936c033b4bd72bfcb5299f982c0b3b9e4
David Okaliwe <okaliwedavid@gmail.com>
2024-12-25 18:58:37 +0000

    Updates

--- src/dashboard/user/Navbar.jsx
+++ src/dashboard/user/Navbar.jsx
@@ -1,9 +1,10 @@
 import { IoSearchOutline } from "react-icons/io5";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <section className='w-full flex items-center z-20 border-b border-b-main7'>
-      <div className="w-full flex items-center py-4 px-8 justify-between">
+      <div className="w-full flex items-center py-5 px-8 justify-between">
         <div className='bg-mainalt w-[50%] rounded-full flex p-3 gap-3
         items-center'>
           <IoSearchOutline
@@ -18,8 +19,29 @@ const Navbar = () => {
           />
         </div>
 
-        <div>
+        <div className="flex items-center gap-4">
+          <div className="bg-mainalt rounded-full">
 
+          </div>
+
+          <div className="bg-mainalt rounded-full">
+
+          </div>
+
+          <div className="flex items-center gap-3">
+            <div className="rounded-full">
+
+            </div>
+
+            <p className="text-[14px] tracking-tight text-main2
+            font-semibold">
+              Peter Alaks
+            </p>
+
+            <MdKeyboardArrowDown
+              className='text-main2 text-[16px] font-medium'
+            />
+          </div>
         </div>
       </div>
     </section>
