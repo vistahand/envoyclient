8e3afba522c0147b0a13dd2de4acc65601e92556
David Okaliwe <okaliwedavid@gmail.com>
2024-12-25 19:48:27 +0000

    Updates

--- src/dashboard/user/Navbar.jsx
+++ src/dashboard/user/Navbar.jsx
@@ -7,7 +7,7 @@ import { profilepic } from "../../assets";
 const Navbar = () => {
   return (
     <section className='w-full flex items-center z-20 border-b border-b-main7'>
-      <div className="w-full flex items-center py-5 px-8 justify-between">
+      <div className="w-full flex items-center py-4 px-8 justify-between">
         <div className='bg-mainalt w-[50%] rounded-full flex p-3 gap-3
         items-center'>
           <IoSearchOutline
@@ -46,12 +46,12 @@ const Navbar = () => {
             />
           </a>
 
-          <div className="flex items-center gap-4 cursor-pointer grow2">
+          <div className="flex items-center gap-4 cursor-pointer">
             <div className="rounded-full overflow-hidden">
               <img
                 src={profilepic}
                 alt="profilepic"
-                className="w-11 h-11 object-cover"
+                className="w-10 h-10 object-cover"
               />
             </div>
 
