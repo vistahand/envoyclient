f51dc93097c8b2422fce3f2d611bfbb3771eac25
David Okaliwe <okaliwedavid@gmail.com>
2024-12-25 18:50:10 +0000

    Updates

--- src/dashboard/user/Navbar.jsx
+++ src/dashboard/user/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { IoSearchOutline } from "react-icons/io5";
 
 const Navbar = () => {
   return (
@@ -6,7 +6,16 @@ const Navbar = () => {
       <div className="w-full flex items-center py-4 px-8 justify-between">
         <div className='bg-mainalt w-[50%] rounded-full flex p-3 gap-3
         items-center'>
+          <IoSearchOutline
+            className='w-[1.3rem] h-auto text-main2'
+          />
 
+          <input
+            type="text"
+            placeholder="Search for anything"
+            className="text-main4 focus:outline-none text-[15px] 
+            placeholder:text-main4 font-semibold tracking-tight bg-transparent"
+          />
         </div>
 
         <div>
