7eb20635e2015000880b71b31a1c180e6981446b
David Okaliwe <okaliwedavid@gmail.com>
2024-12-27 13:05:20 +0000

    Updates

--- src/dashboard/user/Navbar.jsx
+++ src/dashboard/user/Navbar.jsx
@@ -127,93 +127,93 @@ const Navbar = () => {
               )}
             </div>
 
-            <ul className="list-none flex flex-col gap-2.5 mt-14 w-full">
+            <ul className="list-none flex flex-col gap-2 mt-14 w-full">
               {sideLinks.map((link) => (
-                  <li
-                      key={link.id}
-                      className={`${
-                      active === link.title
-                          ? 'bg-primary rounded-lg text-white font-bold'
-                          : 'bg-none text-main2 grow4 font-semibold'
-                      } cursor-pointer text-[16px] tracking-tight`}
-                      onClick={() => {
-                          handleSideItemClick(link);
-                      }}
-                  >
-                      <div className={`p-3 flex gap-4 items-center`}>
-                          <img 
-                              src={link.Icon}
-                              alt={link.id}
-                              className={`w-[1.5rem] h-auto ${
-                                  active === link.title ? 's-white' : 's-main2'
-                              }`}                                  
-                          />
-                          {link.title}
-                      </div>
-                  </li>
+                <li
+                  key={link.id}
+                  className={`${
+                  active === link.title
+                    ? 'bg-primary rounded-lg text-white font-bold'
+                    : 'bg-none text-main2 font-semibold'
+                  } text-[16px] tracking-tight`}
+                  onClick={() => {
+                    handleSideItemClick(link);
+                  }}
+                >
+                  <div className={`p-3 flex ss:gap-4 gap-3 items-center`}>
+                    <img 
+                      src={link.Icon}
+                      alt={link.id}
+                      className={`ss:w-[1.5rem] w-[1.4rem] h-auto ${
+                        active === link.title ? 's-white' : 's-main2'
+                      }`}                                  
+                    />
+                    {link.title}
+                  </div>
+                </li>
               ))}
 
               {/* <li className={`text-[19px] list-item
               ${!session ? 'opacity-50' : 'hover:text-secondary cursor-pointer grow3'} 
               text-textalt mt-20`}>
-                  <div className='flex gap-6 px-5 items-center'
-                  onClick={handleLogout}>
-                      <HiLogout className='transform scale-x-[-1]'
-                      />
-                      Logout
-                  </div>
+                <div className='flex gap-6 px-5 items-center'
+                onClick={handleLogout}>
+                  <HiLogout className='transform scale-x-[-1]'
+                  />
+                  Logout
+                </div>
               </li> */}
             </ul>
 
-            <ul className="list-none flex flex-col gap-2.5 mt-32 
+            <ul className="list-none flex flex-col gap-2 mt-32 
             w-full border-t border-t-main7 pt-12">
-                <li className='text-main2 grow4 font-semibold
-                cursor-pointer text-[16px] tracking-tight'>
-                    <div className={`flex p-3 gap-4 items-center`}>
-                        <img 
-                            src={help}
-                            alt='helpcentre'
-                            className='w-[1.5rem] h-auto'
-                        />
-                        Help Centre
-                    </div>
-                </li>
+              <li className='text-main2 grow4 font-semibold
+              cursor-pointer text-[16px] tracking-tight'>
+                <div className={`flex p-3 ss:gap-4 gap-3 items-center`}>
+                  <img 
+                    src={help}
+                    alt='helpcentre'
+                    className='ss:w-[1.5rem] w-[1.4rem] h-auto'
+                  />
+                  Help Centre
+                </div>
+              </li>
 
-                <li className='text-main2 grow4 font-semibold
-                cursor-pointer text-[16px] tracking-tight'>
-                    <div className={`flex p-3 gap-4 items-center`}>
-                        <img 
-                            src={settings}
-                            alt='settings'
-                            className='w-[1.5rem] h-auto'
-                        />
-                        Settings
-                    </div>
-                </li>
+              <li className='text-main2 grow4 font-semibold
+              cursor-pointer text-[16px] tracking-tight'>
+                <div className={`flex p-3 ss:gap-4 gap-3 items-center`}>
+                  <img 
+                    src={settings}
+                    alt='settings'
+                    className='ss:w-[1.5rem] w-[1.4rem] h-auto'
+                  />
+                  Settings
+                </div>
+              </li>
 
-                <li className='text-logRed grow4 font-semibold
-                cursor-pointer text-[16px] tracking-tight'>
-                    <div className={`flex p-3 gap-4 items-center`}>
-                        <img 
-                            src={logout}
-                            alt='logout'
-                            className='w-[1.5rem] h-auto'
-                        />
-                        Logout
-                    </div>
-                </li>
+              <li className='text-logRed grow4 font-semibold
+              cursor-pointer text-[16px] tracking-tight'>
+                <div className={`flex p-3 ss:gap-4 gap-3 items-center`}>
+                  <img 
+                    src={logout}
+                    alt='logout'
+                    className='ss:w-[1.5rem] w-[1.4rem] h-auto'
+                  />
+                  Logout
+                </div>
+              </li>
             </ul>
 
             <div className="flex flex-col gap-1.5 mt-14 w-full 
             border-t border-t-main7 pt-3">
-                <p className='text-main4 text-[13px] tracking-tight
-                font-medium'>
-                    v. 1.0.1
-                </p>
-              
-                <p className='text-[13px] text-main4 mt-0.5 font-medium tracking-tight'>
-                    ©2024 Envoy Angel Shipping and Logistics Ltd. All Rights Reserved.
-                </p>
+              <p className='text-main4 text-[13px] tracking-tight
+              font-medium'>
+                v. 1.0.1
+              </p>
+            
+              <p className='text-[13px] text-main4 mt-0.5 font-medium tracking-tight'>
+                ©2024 Envoy Angel Shipping and Logistics Ltd. All Rights Reserved.
+              </p>
             </div>
           </div>
         </div>
