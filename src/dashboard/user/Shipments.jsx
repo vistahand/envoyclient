970ca932ce61dd5258bc2f62e1cbc34f4352d3b7
David Okaliwe <okaliwedavid@gmail.com>
2025-01-21 22:51:27 +0000

    Updates

--- src/dashboard/user/Shipments.jsx
+++ src/dashboard/user/Shipments.jsx
@@ -26,7 +26,7 @@ const Shipments = () => {
             ss:text-[16px] text-[14px] md:leading-[1.5rem] ss:leading-[1.5rem] 
             leading-[1.2rem]'>
               Create, view, track and manage all your active and delivered 
-              shipments in one places
+              shipments in one place
             </h4>
           </div>
         
