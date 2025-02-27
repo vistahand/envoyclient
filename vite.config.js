79fe3e03a11df9c4ee3494791958fd47d89abe4e
Uchenna Ofor <ucheofor2015@gmail.com>
2025-02-27 09:44:40 +0100

    fix: update ESLint configuration and enhance Vite config with environment loading

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [react(), svgr()],
-  build: {
-    outDir: "dist",
-    assetsInlineLimit: 4096,
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
-        withCredentials: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), svgr()],
+    build: {
+      outDir: "dist",
+      assetsInlineLimit: 4096,
+    },
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:5000",
+          changeOrigin: true,
+          secure: false,
+          withCredentials: true,
+        },
       },
     },
-  },
+  };
 });
