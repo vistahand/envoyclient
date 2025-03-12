1ced1c856edd237371cef93501283c626807abbe
Declanny <chisomhenryg@gmail.com>
2025-03-12 19:59:53 +0100

    Save current changes before applying stash

--- src/services/api.js
+++ src/services/api.js
@@ -8,6 +8,19 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+// Add a request interceptor to include the token in the headers
+api.interceptors.request.use(
+  (config) => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // Handle token expiration
 api.interceptors.response.use(
