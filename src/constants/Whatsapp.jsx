0a61c11340cf962b940b8e41a3848376bba6f375
Uchenna Ofor <ucheofor2015@gmail.com>
2025-05-02 14:10:23 +0100

    fix: Update location checks in WhatsappLink component for better path handling

--- src/constants/Whatsapp.jsx
+++ src/constants/Whatsapp.jsx
@@ -4,8 +4,20 @@ import { useLocation } from "react-router-dom";
 
 const WhatsappLink = () => {
   const location = useLocation().pathname;
-  const user = location.startsWith("/user");
-  const admin = location.startsWith("/admin");
+  const user = location.startsWith("/user") || location.startsWith("/user/");
+  const admin = location.startsWith("/admin") || location.startsWith("/admin/");
+  const login = location.startsWith("/login") || location.startsWith("/login/");
+  const register =
+    location.startsWith("/register") || location.startsWith("/register/");
+  const verify =
+    location.startsWith("/admin-verify") ||
+    location.startsWith("/admin-verify/");
+  const reset =
+    location.startsWith("/reset-password") ||
+    location.startsWith("/reset-password/");
+  const forgot =
+    location.startsWith("/forgot-password") ||
+    location.startsWith("/forgot-password/");
   const whatsappNumber = import.meta.env.VITE_ADMIN_NUMBER; // Admin's WhatsApp number
 
   const openWhatsApp = () => {
@@ -18,7 +30,9 @@ const WhatsappLink = () => {
       {/* WhatsApp Button */}
       <div
         className={`${
-          admin || user ? "hidden" : "block"
+          admin || user || register || login || verify || reset || forgot
+            ? "hidden"
+            : "block"
         } bg-[#25D366] py-3 px-3 fixed md:bottom-10 z-20
         ss:bottom-8 bottom-6 md:right-20 ss:right-16 right-5 rounded-full 
         transition-opacity duration-400 cursor-pointer grow2 hover:scale-110
